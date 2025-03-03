dbe1578720d742f37065353dad4ce108f4e672cf
Focus <yassin.rabhi@proton.me>
2025-03-03 21:15:50 +0100

    maj

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-# api/index.py (principal point d'entrée pour Vercel)
+# app.py (à la racine)
 from flask import Flask, send_from_directory, request
 import os
 
@@ -11,3 +11,10 @@ def home():
 @app.route('/calendar.ics', methods=['GET'])
 def serve_calendar():
     return send_from_directory('static', 'event.ics')
+
+# Point d'entrée pour Vercel
+@app.route('/api/calendar', methods=['GET'])
+def api_calendar():
+    return serve_calendar()
+
+app=app
